371fbb14fa9e259185f9403fcee701806bc69e02
john stinson <johnbstinson@gmail.com>
2017-11-30 13:26:17 -0600

    added adapter to migrate data from the mobile to the api format

--- PipeTally/PipeTallyMobile/PipeTallyMobile/Services/PipeTallyService.cs
+++ PipeTally/PipeTallyMobile/PipeTallyMobile/Services/PipeTallyService.cs
@@ -9,17 +9,95 @@ using Newtonsoft.Json;
 
 namespace PipeTallyMobile.Services
 {
-    class PipeTallyService
+    public class PipeTallyService
     {
-        public void SendMeasurements(string svcURL, MeasureBatch batch, List<Measurement> measures)
+        public void SendMeasurements(string svcURL, MeasureBatch batch, List<Measurement> measurements)
         {
             var endpoint = svcURL + "/Job";
             HttpClient client = new HttpClient();
-            var data = new Object();
+            var data = Conversion(batch, measurements);
             var json = JsonConvert.SerializeObject(data);
             var content = new StringContent(json);
 
             client.PostAsync(endpoint, content).Start();
         }
+
+        private JobSiteObject Conversion(MeasureBatch measureBatch, List<Measurement> measurements)
+        {
+            var jobSite = new JobSiteObject
+            {
+                Grade = measureBatch.Grade,
+                OuterDiameter = measureBatch.OuterDiameter,
+                InnerDiameter = measureBatch.InnerDiameter,
+                TotalWeight = measureBatch.Weight,
+                ThreadType = measureBatch.TopThread,
+                Measurements = convertMeasurements(measurements)
+
+            };
+            return jobSite;
+        }
+
+        private IEnumerable<MeasurementObject> convertMeasurements(List<Measurement> measurements)
+        {
+            var measurmentObjectList = new List<MeasurementObject>();
+            foreach (var measurement in measurements)
+            {
+                measurmentObjectList.Add(new MeasurementObject
+                {
+                    //IsDamaged = measurement.
+                    ItemDescription = "Casing Joints",
+                    //Joint = measurement.
+                    PipeLength = measurement.FullLength,
+                    ThreadLength = measurement.ThreadLength,
+                    //TopThread = measurement.
+
+                });
+            }
+            return measurmentObjectList;
+        }
+    }
+
+
+    public class MeasurementObject
+    {
+        public int? Joint { get; set; }
+
+        public double? PipeLength { get; set; }
+
+        public double? ThreadLength { get; set; }
+
+        public string ItemDescription { get; set; }
+
+        public string TopThread { get; set; }
+
+        public bool? IsDamaged { get; set; }
+    }
+
+    public class JobSiteObject
+    {
+        public JobSiteObject()
+        {
+            Measurements = new List<MeasurementObject>();
+        }
+
+        public int JobSiteId { get; set; }
+
+        public string Title { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public int? TotalWeight { get; set; }
+
+        public string Grade { get; set; }
+
+        public double? InnerDiameter { get; set; }
+
+        public double? OuterDiameter { get; set; }
+
+        public string ThreadType { get; set; }
+
+        public IEnumerable<MeasurementObject> Measurements { get; set; }
     }
 }
